3f134675e7ac9dbeedc41f3a726f50f3d0e03023
danilo <danilo.mirandha@gmail.com>
2024-07-15 11:31:27 -0300

    feat: add auth module

--- companies/models.py
+++ companies/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Enterprise(models.Model):
     name = models.CharField(max_length=175)
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+
+class Employee(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)

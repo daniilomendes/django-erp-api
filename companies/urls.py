ea8377624ba850448aaf3176e52e6886fbe5bb11
danilo <danilo.mirandha@gmail.com>
2024-07-16 08:03:55 -0300

    feat: add views get permissions in companies

--- companies/urls.py
+++ companies/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from companies.views.employees import Employees, EmployeeDetail
+from companies.views.permissions import PermissionDetail
 
 urlpatterns = [
     # Employees Endpoints
     path('employees', Employees.as_view()),
     path('employees/<int:employee_id>', EmployeeDetail.as_view()),
 
-    
+    # Groups and Permissions Endpoints
+    path('permissions', PermissionDetail.as_view())
+
 ]
